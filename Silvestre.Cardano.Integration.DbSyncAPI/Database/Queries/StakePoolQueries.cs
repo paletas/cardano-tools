5b59fed0afa79dd29dc57076f60e2b7872c03b16
João Silvestre <joaopaulosilvestre@hotmail.com>
2021-05-14 23:41:57 +0100

    Revert "Still working on issues with the stakepool queries"
    This reverts commit 230d5e5400dbb97b63a6532ecc5ee7c5a9b6bc69.

--- Silvestre.Cardano.Integration.DbSyncAPI/Database/Queries/StakePoolQueries.cs
+++ Silvestre.Cardano.Integration.DbSyncAPI/Database/Queries/StakePoolQueries.cs
@@ -13,22 +13,17 @@ namespace Silvestre.Cardano.Integration.DbSyncAPI.Database.Queries
         {
             const string QueryString =
 				@"SELECT pool_hash.view AS PoolAddress,
-					pool_information.pledge AS Pledge, pool_information.active_epoch_no AS ActiveSinceEpochNumber,
-					pool_information.margin AS Margin, pool_information.fixed_cost AS FixedCost,
-					pool_information.url AS MetadataUrl,
+					pool_update.pledge AS Pledge, pool_update.active_epoch_no AS ActiveSinceEpochNumber,
+					pool_update.margin AS Margin, pool_update.fixed_cost AS FixedCost,
+					pool_meta_data.url AS MetadataUrl,
 					pool_retire.retiring_epoch AS RetiringEpoch,
 					COUNT(*) OVER () AS TotalStakePools
 				FROM public.pool_hash
-					INNER JOIN LATERAL (
-						SELECT pool_update.hash_id, pool_update.pledge, pool_update.active_epoch_no, pool_update.margin, pool_update.fixed_cost, pool_meta_data.url
-						FROM public.pool_update
-							INNER JOIN public.pool_meta_data ON pool_update.registered_tx_id = pool_meta_data.registered_tx_id
-						WHERE pool_hash.id = pool_update.hash_id AND pool_update.active_epoch_no <= (SELECT MAX(no) FROM public.epoch)
-						ORDER BY pool_update.active_epoch_no DESC
-						LIMIT 1
-					) pool_information ON TRUE
+					INNER JOIN public.pool_update ON pool_hash.id = pool_update.hash_id
+					INNER JOIN public.pool_meta_data ON pool_update.registered_tx_id = pool_meta_data.registered_tx_id
+					INNER JOIN public.pool_relay ON pool_relay.update_id = pool_update.id
 					LEFT JOIN public.pool_retire ON pool_retire.hash_id = pool_hash.id
-				WHERE (pool_retire.retiring_epoch IS NULL OR pool_retire.retiring_epoch >= (SELECT MAX(no) FROM public.epoch))
+				WHERE pool_retire.retiring_epoch IS NULL OR pool_retire.retiring_epoch >= (SELECT MAX(no) FROM public.epoch)
 				ORDER BY pool_hash.id ASC
 				LIMIT @Limit
 				OFFSET @Offset";
@@ -42,22 +37,18 @@ namespace Silvestre.Cardano.Integration.DbSyncAPI.Database.Queries
         {
             const string QueryString =
 				@"SELECT pool_hash.view AS PoolAddress,
-					pool_information.pledge AS Pledge, pool_information.active_epoch_no AS ActiveSinceEpochNumber,
-					pool_information.margin AS Margin, pool_information.fixed_cost AS FixedCost,
-					pool_information.url AS MetadataUrl,
+					pool_update.pledge AS Pledge, pool_update.active_epoch_no AS ActiveSinceEpochNumber,
+					pool_update.margin AS Margin, pool_update.fixed_cost AS FixedCost,
+					pool_meta_data.url AS MetadataUrl,
 					pool_retire.retiring_epoch AS RetiringEpoch
 				FROM public.pool_hash
-					INNER JOIN LATERAL (
-						SELECT pool_update.hash_id, pool_update.pledge, pool_update.active_epoch_no, pool_update.margin, pool_update.fixed_cost, pool_meta_data.url
-						FROM public.pool_update
-							INNER JOIN public.pool_meta_data ON pool_update.registered_tx_id = pool_meta_data.registered_tx_id
-						WHERE pool_hash.id = pool_update.hash_id AND pool_update.active_epoch_no <= (SELECT MAX(no) FROM public.epoch)
-						ORDER BY pool_update.active_epoch_no DESC
-						LIMIT 1
-					) pool_information ON TRUE
+					INNER JOIN public.pool_update ON pool_hash.id = pool_update.hash_id
+					INNER JOIN public.pool_meta_data ON pool_update.registered_tx_id = pool_meta_data.registered_tx_id
+					INNER JOIN public.pool_relay ON pool_relay.update_id = pool_update.id
 					LEFT JOIN public.pool_retire ON pool_retire.hash_id = pool_hash.id
 				WHERE (pool_retire.retiring_epoch IS NULL OR pool_retire.retiring_epoch >= (SELECT MAX(no) FROM public.epoch))
-					AND pool_hash.view = @PoolAddress";
+					AND pool_hash.view = @PoolAddress
+				ORDER BY pool_hash.id ASC";
 
             return await dbConnection.QuerySingleOrDefaultAsync<StakePool>(QueryString, new { PoolAddress = poolAddress }, commandType: System.Data.CommandType.Text).ConfigureAwait(false);
         }
